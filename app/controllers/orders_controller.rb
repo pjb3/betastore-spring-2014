e772b6ec0b1e847629e3693f29cfd35d0a7c972d
Paul Barry <mail@paulbarry.com>
2014-08-25 21:04:45 -0400

    Order form WIP

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,19 @@ class OrdersController < ApplicationController
     @order.build_shipping_address
   end
 
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
   def update
     cart_params = params[:order][:line_items_attributes]
     logger.debug "cart_params: #{cart_params.values.inspect}"
@@ -21,5 +34,16 @@ class OrdersController < ApplicationController
     session[:cart] = new_cart
 
     redirect_to cart_path, notice: 'Your cart was updated'
+  end
+
+private
+  def order_params
+    params.permit(
+      :line_items_attributes =>
+        [:product_id, :quantity],
+      :credit_card_attributes =>
+        [:card_number],
+      :shipping_address_attributes =>
+        [:street, :city, :state, :zip])
   end
 end
